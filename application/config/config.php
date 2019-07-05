d835c090067721b51a9367e0898bd7552a92e9fd
sherlymahdiati <40010664+sherlymahdiati@users.noreply.github.com>
2019-07-05 16:20:14 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Visin_2019/';
+$config['base_url'] = 'https://sherlymahdiati.github.io/Visin_2019/';
 
 /*
 |--------------------------------------------------------------------------
